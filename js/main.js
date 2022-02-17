2bd9770426b7076d4fd60fc18ce064c2407467d8
Denis <dgorichan@gmail.com>
2022-02-17 13:06:11 +0200

    Lesson-2

---
+++ js/main.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/*============================== TASK-1 =============================== */
+let user = {};
+
+user.name = "John";
+user.surname = "Smith";
+
+user.name  = "Pete";
+
+delete user.name;
+
+console.log(user);
+/*============================== TASK-1 =============================== */
+
+/*============================== TASK-3 =============================== */
+let salaries = {
+
+    John: 100,
+    
+    Ann: 160,
+    
+    Pete: 130
+    
+    }
+
+let sum = salaries.John + salaries.Ann + salaries.Pete;
+
+console.log(sum);
+/*============================== TASK-3 =============================== */
